192a0a2025991c6953f4caf53be32d4e1f5c74dd
xiaoha798 <hadrien.vanlierde@hotmail.com>
2020-07-15 11:09:25 +0800

    update 2

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ setuptools.setup(
     description="Prediction of gender probabilities for English names.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/xiaoha798/py-NameGenderPredictor",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
